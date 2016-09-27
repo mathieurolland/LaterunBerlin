0eb5a2265902a8295fd2d34622d6a87becd7a371
louis pons <louis.pons@icloud.com>
2016-09-27 20:10:22 +0200

    races controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  get 'races/index'
+
+  get 'races/show'
+
   devise_for :users
   root to: 'pages#home'
   get "/dashboard", to: "pages#dashboard"
 
-  resources :races, only [:index, :show] do
-    resources :review, only [:create, :destroy]
-    resources :favorite_races, only [:create, :destroy]
-    resources :bibs, only [:new, :create, :update, :edit, :destroy] do
-      resources :orders, only [:create, :new, :show, :update, :index] do
-        resources :messages, only [:create, :destroy]
+  resources :races, only: [:index, :show] do
+    resources :reviews, only: [:create, :destroy]
+    resources :favorite_races, only: [:create, :destroy]
+    resources :bibs, only: [:new, :create, :update, :edit, :destroy] do
+      resources :orders, only: [:create, :new, :show, :update, :index] do
+        resources :messages, only: [:create, :destroy]
       end
     end
   end
-
-
 end
