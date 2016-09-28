70b989fa58ae6393a82b77ac288c260099be0fcc
louis pons <louis.pons@icloud.com>
2016-09-28 14:42:36 +0200

    first controller implementation, basic stuff

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,7 +1,18 @@
 class RacesController < ApplicationController
   def index
+    @races = Race.all
   end
 
   def show
+  end
+
+  private
+
+  # def race_params
+  #   params[:race].permit(:name, :date, :address, :distance)
+  # end
+
+  def find_race
+    @race = Race.find(params[:id])
   end
 end
