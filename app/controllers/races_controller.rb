7a21055efd72bd360e80f81ab608a5e06d885e42
louis pons <louis.pons@icloud.com>
2016-10-31 09:29:07 +0100

    small change on a method race_controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -12,7 +12,7 @@ class RacesController < ApplicationController
   #   params[:race].permit(:name, :date, :address, :distance)
   # end
 
-  def find_race
-    @race = Race.find(params[:id])
-  end
+  # def find_race
+  #   @race = Race.find(params[:id])
+  # end
 end
